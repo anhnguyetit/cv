90a18bb38ca66958cf266536a2a7bb1a71ba359c
anhnguyetit <ntan100199@gmail.com>
2020-12-30 16:59:06 +0700

    Updates

---
+++ precache-manifest.1bccb29b638dc590487a39c4035a6ee9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcabd0ed064f9741c7c0d785a543348",
+    "url": "/my_cv/index.html"
+  },
+  {
+    "revision": "bf32ef0eee9a99726251",
+    "url": "/my_cv/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "4bb4f331c8bfc9d97b4d",
+    "url": "/my_cv/static/css/main.dbd8b7ae.chunk.css"
+  },
+  {
+    "revision": "bf32ef0eee9a99726251",
+    "url": "/my_cv/static/js/2.11724561.chunk.js"
+  },
+  {
+    "revision": "643658cfb6185d6a12d22f5eb6443bf5",
+    "url": "/my_cv/static/js/2.11724561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb4f331c8bfc9d97b4d",
+    "url": "/my_cv/static/js/main.61b67e1a.chunk.js"
+  },
+  {
+    "revision": "b707fad9b14152fb622c",
+    "url": "/my_cv/static/js/runtime-main.94732353.js"
+  },
+  {
+    "revision": "3f529d0c7a60251f0331223e75fb8035",
+    "url": "/my_cv/static/media/profile.3f529d0c.png"
+  }
+]);
